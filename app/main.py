07709e833ab37f80c41e5e1a854f64a4c23bd3ce
DiWILove <povilaitusdiana@gmail.com>
2025-06-24 11:37:51 +0300

    flake8_debug1

--- app/main.py
+++ app/main.py
@@ -4,11 +4,12 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
+        self.wife = None
+        self.husband = None
         Person.people[name] = self
 
 
 def create_person_list(people_list: list) -> list:
-
     person_objects = []
     for person_dict in people_list:
         person = Person(name=person_dict["name"], age=person_dict["age"])
@@ -16,11 +17,16 @@ def create_person_list(people_list: list) -> list:
 
     for person_dict in people_list:
         name = person_dict["name"]
-        current_person = Person.people[name]
+        current = Person.people[name]
 
         if "wife" in person_dict and person_dict["wife"] is not None:
-            current_person.wife = Person.people[person_dict["wife"]]
+            spouse = Person.people[person_dict["wife"]]
+            current.wife = spouse
+            spouse.husband = current
+
         elif "husband" in person_dict and person_dict["husband"] is not None:
-            current_person.husband = Person.people[person_dict["husband"]]
+            spouse = Person.people[person_dict["husband"]]
+            current.husband = spouse
+            spouse.wife = current
 
     return person_objects
